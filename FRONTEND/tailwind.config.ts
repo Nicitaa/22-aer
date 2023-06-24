863e3ca49182322b8f01673fe27a5669b14a9719
Nicitaa <icpcsenondaryemail@gmail.com>
2023-06-24 13:33:07 +0200

    configure tailwind.confing.ts

--- FRONTEND/tailwind.config.ts
+++ FRONTEND/tailwind.config.ts
@@ -4,32 +4,28 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      primary: ["Inter", "sans-serif"],
-      secondary: ["Proxima Nova", "sans-serif"],
+      "font-primary": ["Inter", "sans-serif"],
+      "font-secondary": ["Proxima Nova", "sans-serif"],
     },
     fontSize: {
-      xs: ".75rem", // Extra small
-      sm: ".875rem", // Small
-      base: "1rem", // Base
-      lg: "1.125rem", // Large
-      xl: "1.25rem", // Extra large
-      "2xl": "1.5rem", // 2 Extra large
-      "3xl": "3rem",
-      "4xl": "4rem",
+      xs: "1rem", // Extra small
+      small: "1.25rem", // Small
+      medium: "1.5rem", // Base
+      big: "4rem", // Large
     },
     screens: {
       tablet: "700px",
       // => @media (min-width: 700px) { ... }
     },
     extend: {
-      colors: {
-        primary: "rgba(255, 255, 255, 0.9)",
-        secondary: "rgba(255, 255, 255, 0.4)",
-        cta: "rgba(131, 173, 175, 1)",
-        "bg-secondary": "rgba(7, 7, 10, 0.4)",
-      },
       backgroundImage: {
         "bg-primary": "url('/background.png')",
+      },
+      colors: {
+        primary: "rgba(255, 255, 255, 0.9)",
+        "primary-darker": "rgba(255,255,255,0.4)",
+        secondary: "rgba(7, 7, 10, 0.4)",
+        cta: "rgba(131, 173, 175, 1)",
       },
     },
   },
