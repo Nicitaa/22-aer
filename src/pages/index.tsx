0027394b9fb14051479769c71ae39c32a1bed1c8
Nicitaa <39565703+Nicitaa@users.noreply.github.com>
2023-06-27 16:13:42 +0200

    home-add-page-25.06.23 (#8)
    * homepage created
    
    * save for reload
    
    * home-add-page-23.06.25

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import Head from "next/head";
-import Link from "next/link";
-import React from "react";
+import { Container, Slider, SliderCounter } from "~/components"
 
 export default function Home() {
   return (
     <>
-      
+      <div className="text-center w-full h-[90%] flex flex-col gap-2 justify-between">
+        <Container>
+          <h1 className="font-primary text-big font-bold">The best <br /> bag behind you</h1>
+          <p className="font-secondary text-small text-primary-darker">We provide high quality for each bag. In our shop you can find bag that fits your needs - it can be colorfull / spacious / waterproof bag - what is yours? - you may create your own! </p>
+        </Container>
+
+        <Container>
+          <SliderCounter />
+        </Container>
+
+        <Slider />
+      </div>
     </>
   );
 }
