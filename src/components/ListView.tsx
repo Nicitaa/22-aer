9f82d698f2e48cd510c3257ee42cd46602f80926
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-06-29 23:00:59 +0300

    Button alignment

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,25 +1,21 @@
-/* eslint-disable @next/next/no-img-element */
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
 type Product = {
-  name: string;
-  price: number;
-  id: number;
-  description: string;
-  image: string;
-};
+  name: string
+  price: number
+  id: number
+  description: string
+  image: string
+}
 const ListView = ({ products }: { products: Product[] }) => {
   return (
     <div className="scrollbar md:grid-cols-2 grid max-h-[750px] grid-cols-1 gap-12 overflow-y-scroll">
       <section className=" grid gap-y-12">
         {products.map((product) => {
-          const { id, name, price, description, image } = product;
+          const { id, name, price, description, image } = product
           return (
-            <article
-              key={id}
-              className="flex flex-col laptop:gap-12 gap-6 px-4 tablet:flex-row tablet:justify-center "
-            >
+            <article key={id} className="flex flex-col laptop:gap-12 gap-6 px-4 tablet:flex-row tablet:justify-center pt-4 ">
               <Image
                 className=" duration-300 group-hover:scale-125"
                 src={image}
@@ -31,27 +27,20 @@ const ListView = ({ products }: { products: Product[] }) => {
                 objectPosition="center"
               />
 
-              <div className="font-primary text-white">
+              <div className="font-primary text-white relative">
                 <h4 className="mb-2 font-bold capitalize text-lg">{name}</h4>
-                <h5 className="mb-3 font-primary font-bold text-sm text-cta">
-                  ${(price / 100).toFixed(2)}
-                </h5>
-                <p className="mb-4 max-w-lg">
-                  {description.substring(0, 150)}...
-                </p>
-                <Link
-                  href={`/products/${id}`}
-                  className=" bg-cta px-2 py-1 text-xs"
-                >
+                <h5 className="mb-3 font-primary font-bold text-sm text-cta">${(price / 100).toFixed(2)}</h5>
+                <p className="mb-4 max-w-lg">{description.substring(0, 150)}...</p>
+                <Link href={`/products/${id}`} className=" bg-cta px-2 py-1 text-xs absolute right-0 tablet:static">
                   Details
                 </Link>
               </div>
             </article>
-          );
+          )
         })}
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default ListView;
+export default ListView
