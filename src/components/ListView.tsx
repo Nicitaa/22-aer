183b0ce019e305958079285d205bab74187caa9b
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-07-01 15:47:51 +0300

    alignment improvements applied

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -15,7 +15,10 @@ const ListView = ({ products }: { products: Product[] }) => {
         {products.map((product) => {
           const { id, name, price, description, image } = product
           return (
-            <article key={id} className="flex flex-col laptop:gap-12 gap-6 px-4 tablet:flex-row tablet:justify-center pt-4 ">
+            <article
+              key={id}
+              className="flex flex-col laptop:gap-12 gap-6 px-8 tablet:flex-row tablet:justify-center pt-4 "
+            >
               <Image
                 className=" duration-300 group-hover:scale-125"
                 src={image}
@@ -26,12 +29,16 @@ const ListView = ({ products }: { products: Product[] }) => {
                 objectFit="cover"
                 objectPosition="center"
               />
-
               <div className="font-primary text-white relative">
-                <h4 className="mb-2 font-bold capitalize text-lg">{name}</h4>
-                <h5 className="mb-3 font-primary font-bold text-sm text-cta">${(price / 100).toFixed(2)}</h5>
+                <div className="tablet:block flex justify-between">
+                  <h4 className="mb-2 font-bold capitalize text-lg">{name}</h4>
+                  <h5 className="mb-3 font-primary font-bold text-sm tablet:text-cta">${(price / 100).toFixed(2)}</h5>
+                </div>
                 <p className="mb-4 max-w-lg">{description.substring(0, 150)}...</p>
-                <Link href={`/products/${id}`} className=" bg-cta px-2 py-1 text-xs absolute right-0 tablet:static">
+                <Link
+                  href={`/products/${id}`}
+                  className=" bg-cta px-2 py-1 text-xs absolute tablet:w-16 w-full  text-center right-0 tablet:static"
+                >
                   Details
                 </Link>
               </div>
