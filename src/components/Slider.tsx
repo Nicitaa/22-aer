8bc61f3a768834b04e054cf5149cad397978f658
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-05 16:55:21 +0200

    for save

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,11 @@
 import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi"
+import { topSales } from '../constant/topSales'
+import Image from 'next/image'
+
+
+interface ITopSales {
+  imgSrc: string
+}[]
 
 export function Slider() {
   return (
@@ -11,11 +18,14 @@ export function Slider() {
       <h1 className="font-primary font-bold text-medium">Top Sales</h1>
 
       <div className="absolute top-1/2 -translate-y-1/2 flex flex-row items-center gap-2">
+        {topSales.map((image: ITopSales) => (
+          <Image src={image.imgSrc} alt='image' width={200} height={200} key={image.imgSrc} />
+        ))}
         <div className="absolute -left-[30vw] w-[25vh] h-[10vh] bg-primary-darker" />
-        <PiCaretLeftBold size={48} />
         <div className="w-[50vw] h-[20vh] bg-primary-darker" />
+
+        <PiCaretLeftBold size={48} />
         <PiCaretRightBold size={48} />
-        <div className="absolute -right-[30vw] w-[25vh] h-[10vh] bg-primary-darker" />
       </div>
 
     </div>
---
+++ src/constant/topSales.ts
@@ -0,0 +1,17 @@
+export const topSales = [
+  {
+    imgSrc: "/top-sales/top-sales-1.jpg"
+  },
+  {
+    imgSrc: "/top-sales/top-sales-2.jpg"
+  },
+  {
+    imgSrc: "/top-sales/top-sales-3.jpg"
+  },
+  {
+    imgSrc: "/top-sales/top-sales-4.jpg"
+  },
+  {
+    imgSrc: "/top-sales/top-sales-5.jpg"
+  }
+]
