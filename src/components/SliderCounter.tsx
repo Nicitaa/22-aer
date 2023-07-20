9506df424682648ab5685fc96675c170960a2088
Nicitaa <icpcsenondaryemail@gmail.com>
2023-07-20 21:07:12 +0200

    progressbarClassName in props

--- src/components/SliderCounter.tsx
+++ src/components/SliderCounter.tsx
@@ -1,8 +1,14 @@
 "use client"
 import { useState } from "react"
 
+interface ISliderCounter {
+  className?: string
+  array: any
+  progressbarClassName?: string
+  progressfillClassname?: string
+}
 
-export function SliderCounter(data: { className?: string, array: any }) {
+export function SliderCounter(data: ISliderCounter) {
   const slidesLength = data.array.length
   const [currentIndex, setCurrentIndex] = useState(Math.floor((slidesLength / 2)))
   let percent = (100 * (currentIndex + 1)) / slidesLength;
@@ -19,9 +25,9 @@ export function SliderCounter(data: { className?: string, array: any }) {
       <h1 className="font-bold ml-1">/0{currentIndex + 1}</h1>
 
       <div>
-        <div className="w-full laptop:w-[400px]
-         mx-auto laptop:ml-0 rounded-md bg-secondary border-2 border-solid border-secondary overflow-hidden">
-          <div style={{ width: `${percent}%` }} className={` h-[3px] bg-cta transition-all duration-500 ease-in-out`} />
+        <div className={`${data.progressbarClassName}
+         mx-auto laptop:ml-0 rounded-md border-2 border-solid overflow-hidden`}>
+          <div style={{ width: `${percent}%` }} className={` ${data.progressfillClassname} transition-all duration-500 ease-in-out`} />
         </div>
 
         <div className="px-1">
