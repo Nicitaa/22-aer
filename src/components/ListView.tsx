959acb1356d92ad1508a32854f361f53efba54d6
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-07-24 21:03:05 +0300

    components alignment fixed

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -17,19 +17,16 @@ const ListView = ({ products }: { products: Product[] }) => {
         {products.map((product) => {
           const { id, preview, title, subTitle, price } = product
           return (
-            <article
-              key={id}
-              className="relative flex flex-col laptop:gap-12 w-full gap-6 px-4 tablet:flex-row tablet:justify-between "
-            >
+            <article key={id} className="relative flex flex-col laptop:gap-10 w-full gap-6 px-4 tablet:flex-row  ">
               <Image className="tablet:w-[55%]" src={preview ?? ""} alt="Placeholder Image" width={600} height={480} />
 
-              <div className="font-primary text-white tablet:w-[35%]">
+              <div className="font-primary text-white tablet:w-[35%] tablet:relative">
                 <h4 className="mb-2 font-bold capitalize text-lg">{title}</h4>
                 <h5 className="mb-3 font-primary font-bold text-sm text-cta">${(price / 100).toFixed(2)}</h5>
                 <p className="mb-4 max-w-lg">{subTitle.substring(0, 150)}...</p>
                 <Link
                   href={`/product?id=${id}`}
-                  className=" bg-cta px-2 py-1 text-xs absolute tablet:w-16 w-[90%]  text-center right-auto tablet:static"
+                  className=" bg-cta px-2 py-1 text-xs absolute tablet:w-20 w-[90%]  text-center right-auto tablet:bottom-0 tablet:right-0"
                 >
                   Details
                 </Link>
