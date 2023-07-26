015512f3087c779edfe19a6c2677e40f0fac40a4
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-07-26 15:03:07 +0300

    products-imp-listViewLayout

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 type Product = {
@@ -10,26 +9,60 @@ type Product = {
   price: number
   imagesUrl: string[]
 }
+
 const ListView = ({ products }: { products: Product[] }) => {
+  const componentRef = useRef<HTMLDivElement>(null)
+  const [componentWidth, setComponentWidth] = useState<number>(0)
+  useEffect(() => {
+    const initialWidth = componentRef.current?.offsetWidth
+    setComponentWidth(initialWidth || 0)
+  }, [])
   return (
-    <div className="scrollbar md:grid-cols-2 grid max-h-[750px] grid-cols-1 gap-12 overflow-y-scroll">
+    <div
+      ref={componentRef}
+      className="scrollbar md:grid-cols-2 grid max-h-[750px] grid-cols-1 gap-12 overflow-y-scroll"
+    >
       <section className=" grid gap-y-12 justify-items-center ">
         {products.map((product) => {
           const { id, preview, title, subTitle, price } = product
           return (
-            <article key={id} className="relative flex flex-col laptop:gap-10 w-full gap-6 px-4 tablet:flex-row  ">
+            <article
+              key={id}
+              className="relative flex flex-col laptop:gap-6 w-full gap-6 tablet:px-8 tablet:flex-row  "
+            >
               <Image className="tablet:w-[55%]" src={preview ?? ""} alt="Placeholder Image" width={600} height={480} />
 
               <div className="font-primary text-white tablet:w-[35%] tablet:relative">
-                <h4 className="mb-2 font-bold capitalize text-lg">{title}</h4>
-                <h5 className="mb-3 font-primary font-bold text-sm text-cta">${(price / 100).toFixed(2)}</h5>
-                <p className="mb-4 max-w-lg">{subTitle.substring(0, 150)}...</p>
-                <Link
-                  href={`/product?id=${id}`}
-                  className=" bg-cta px-2 py-1 text-xs absolute tablet:w-20 w-[90%]  text-center right-auto tablet:bottom-0 tablet:right-0"
-                >
-                  Details
-                </Link>
+                <h4 className="font-bold capitalize text-lg">{title}</h4>
+                {componentWidth > 700 ? (
+                  <h5 className="font-primary font-bold text-md text-cta">${(price / 100).toFixed(2)}</h5>
+                ) : (
+                  <></>
+                )}
+                <p className="max-w-lg text-md">{subTitle.substring(0, 150)}...</p>
+                {componentWidth > 700 ? (
+                  <Link
+                    href={`/product?id=${id}`}
+                    className=" bg-cta px-2 py-1 text-xs absolute tablet:w-20 w-full  text-center right-auto tablet:bottom-0 tablet:right-0"
+                  >
+                    Details
+                  </Link>
+                ) : (
+                  <div className="w-full flex gap-8 mt-4">
+                    <Link
+                      href={`/product?id=${id}`}
+                      className=" bg-cta px-2 py-1 text-md font-bold flex-grow  text-center rounded-md "
+                    >
+                      Buy ${(price / 100).toFixed(2)}
+                    </Link>
+                    <Link
+                      href={`/product?id=${id}`}
+                      className=" bg-cta px-2 py-1 text-md font-bold flex-grow  text-center rounded-md "
+                    >
+                      Details
+                    </Link>
+                  </div>
+                )}
               </div>
             </article>
           )
