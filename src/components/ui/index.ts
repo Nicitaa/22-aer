111e3a108a645206f49f89ba79af58b817320e28
arifm6 <arifmassih6@gmail.com>
2023-08-02 11:16:18 -0400

    deleted SimpleCheckbox component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,4 @@
 import { RadioButton } from "./RadioButton"
 import { Input } from "./Input"
 import { Checkbox } from "./Checkbox"
-import { SimpleCheckbox } from "./SimpleCheckbox"
-export { RadioButton, Input, Checkbox, SimpleCheckbox }
+export { RadioButton, Input, Checkbox }
