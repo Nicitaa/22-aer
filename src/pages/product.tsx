182b032b042a945886400d924891bebb970d4156
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-08-09 23:27:14 +0300

    image slider fixed

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,11 +1,10 @@
 "use client"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import Image from "next/image"
 import bags from "~/constant/bags.json"
 
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
-import router from "next/router"
 import { useRouter } from "next/router"
 import { priceFormatter } from "~/utils/priceFormatter"
 type FoundProduct =
@@ -23,13 +22,12 @@ const Product = () => {
   const [foundProduct, setFoundProduct] = useState<FoundProduct>(undefined)
   const [quantity, setQuantity] = useState(1)
   const [currentImage, setCurrentImage] = useState(0)
-
+  const [sliderXPosition, setSliderXPosition] = useState(0)
   useEffect(() => {
     const { id } = router.query
     if (id) {
       const product = bags?.find(bag => bag.id === id)
       setFoundProduct(product)
-      setCurrentImage(Math.floor(product?.imagesUrl?.length ? product?.imagesUrl?.length / 2 : 0))
     }
   }, [router.query])
 
@@ -40,47 +38,67 @@ const Product = () => {
   const { title, subTitle, price, imagesUrl } = foundProduct
   return (
     <div className=" h-[85vh]">
-      <section className="relative laptop:flex mx-auto my-0 laptop:h-[80%]   w-full max-w-[1384px] laptop:px-8 tablet:px-0">
-        <div className="laptop:w-1/2 h-full w-full px-8 laptop:px-0">
+      <section className=" laptop:flex mx-auto my-0 laptop:h-[80%] h-[40%]   w-full max-w-[1384px] laptop:px-8 tablet:px-0">
+        <div className="laptop:w-1/2 h-full w-full px-8 laptop:px-0 relative">
           <Image
             className=" duration-300 w-full h-full object-cover object-center"
-            src={imagesUrl[currentImage] ?? ""}
+            src={imagesUrl[currentImage] ?? imagesUrl[0] ?? ""}
             width={800}
             height={1400}
             alt="Placeholder Image"
           />
-          <div className="flex absolute laptop:w-1/2 w-10/12  laptop:h-[80px] h-[60px] mt-6">
-            <div className="flex w-full items-center justify-between">
-              <IoIosArrowBack
-                className="h-[30px] laptop:h-[80px] w-auto hover:fill-cta active:scale-150 duration-300"
-                onClick={() => {
-                  setCurrentImage(prev => (prev === 0 ? imagesUrl.length - 1 : prev - 1))
-                }}
-              />
-              {imagesUrl.map((imageUrl, index) => {
-                return (
-                  <Image
-                    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                    className={`duration-300  w-16 laptop:w-24 ${currentImage === index && "scale-125"}`}
-                    src={imageUrl ?? ""}
-                    width={100}
-                    height={800}
-                    alt="Placeholder Image"
-                    key={index}
-                    layout="intrinsic"
-                    objectFit="cover"
-                    objectPosition="center"
-                    onClick={() => setCurrentImage(index)}
-                  />
+          <div className="flex justify-between  items-center h-[15vh] tablet:mt-0 mt-3">
+            <button
+              onClick={() => {
+                const vh = document.getElementById("__next")?.getBoundingClientRect().width
+                setSliderXPosition(
+                  currentImage !== imagesUrl.length - 1
+                    ? vh ?? 0 > 1024
+                      ? -currentImage * 144 + 36
+                      : -currentImage * 96 + 12
+                    : vh ?? 0 > 1024
+                    ? -imagesUrl.length - 1 * 144 + 36
+                    : -imagesUrl.length - 1 * 96 + 12
                 )
-              })}
-              <IoIosArrowForward
-                className="h-[30px] laptop:h-[80px] w-auto hover:fill-cta active:scale-150 duration-300"
-                onClick={() => {
-                  setCurrentImage(prev => (prev === imagesUrl.length - 1 ? 0 : prev + 1))
-                }}
-              />
+                setCurrentImage(prev => (prev === 0 ? imagesUrl.length - 1 : prev - 1))
+              }}>
+              {
+                <IoIosArrowBack className="h-[30px] laptop:h-[80px] w-auto hover:fill-cta active:scale-150 duration-300" />
+              }
+            </button>
+            <div className="  overflow-hidden  w-full">
+              <div className="flex tablet:gap-9 gap-3" style={{ transform: `translateX(${sliderXPosition}px)` }}>
+                {imagesUrl.map((imageUrl, index) => {
+                  return (
+                    <Image
+                      className={`duration-300 tablet:w-36 w-24 ${currentImage === index ? " scale-150" : ""}`}
+                      src={imageUrl ?? ""}
+                      width={400}
+                      height={300}
+                      alt="Placeholder Image"
+                      key={index}
+                      onClick={() => setCurrentImage(index)}
+                    />
+                  )
+                })}
+              </div>
             </div>
+            <button
+              onClick={() => {
+                const vh = document.getElementById("__next")?.getBoundingClientRect().width
+                setCurrentImage(prev => (prev === imagesUrl.length - 1 ? 0 : prev + 1))
+                setSliderXPosition(
+                  currentImage !== imagesUrl.length - 1
+                    ? vh ?? 0 > 1024
+                      ? -currentImage * 144 + 36
+                      : -currentImage * 96 + 12
+                    : 0
+                )
+              }}>
+              {
+                <IoIosArrowForward className="h-[30px] laptop:h-[80px] w-auto hover:fill-cta active:scale-150 duration-300" />
+              }
+            </button>
           </div>
         </div>
         {/* About item container */}
