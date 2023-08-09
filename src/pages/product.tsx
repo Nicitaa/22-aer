6c446f4cdbe3d262d55b0182bcf14745436f3c98
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-08-09 18:49:16 +0300

    product description layout fixed

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,35 +1,53 @@
-import React, { useState } from "react"
+"use client"
+import React, { useEffect, useState } from "react"
 import Image from "next/image"
 import bags from "~/constant/bags.json"
 
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
 import router from "next/router"
-
+import { useRouter } from "next/router"
+import { priceFormatter } from "~/utils/priceFormatter"
+type FoundProduct =
+  | {
+      id: string
+      preview: string
+      title: string
+      subTitle: string
+      price: number
+      imagesUrl: string[]
+    }
+  | undefined
 const Product = () => {
-  const { id } = router.query
-  const foundProduct = bags?.find(bag => bag.id === id)
+  const router = useRouter()
+  const [foundProduct, setFoundProduct] = useState<FoundProduct>(undefined)
   const [quantity, setQuantity] = useState(1)
+  const [currentImage, setCurrentImage] = useState(0)
+
+  useEffect(() => {
+    const { id } = router.query
+    if (id) {
+      const product = bags?.find(bag => bag.id === id)
+      setFoundProduct(product)
+      setCurrentImage(Math.floor(product?.imagesUrl?.length ? product?.imagesUrl?.length / 2 : 0))
+    }
+  }, [router.query])
 
   if (!foundProduct) {
     return <h1 className="mx-auto text-lg text-center">Loading...</h1>
   }
+
   const { title, subTitle, price, imagesUrl } = foundProduct
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [currentImage, setCurrentImage] = useState(Math.floor(imagesUrl.length / 2))
   return (
-    <div className=" h-full min-h-screen">
-      <section className="relative  laptop:flex mx-auto my-0  w-full max-w-[1284px] laptop:px-8 tablet:px-0">
-        <div className="laptop:w-1/2 w-full px-8 laptop:px-0">
+    <div className=" h-[85vh]">
+      <section className="relative laptop:flex mx-auto my-0 laptop:h-[80%]   w-full max-w-[1384px] laptop:px-8 tablet:px-0">
+        <div className="laptop:w-1/2 h-full w-full px-8 laptop:px-0">
           <Image
-            className=" duration-300"
+            className=" duration-300 w-full h-full object-cover object-center"
             src={imagesUrl[currentImage] ?? ""}
-            width={400}
-            height={400}
+            width={800}
+            height={1400}
             alt="Placeholder Image"
-            layout="responsive"
-            objectFit="cover"
-            objectPosition="center"
           />
           <div className="flex absolute laptop:w-1/2 w-10/12  laptop:h-[80px] h-[60px] mt-6">
             <div className="flex w-full items-center justify-between">
@@ -43,7 +61,7 @@ const Product = () => {
                 return (
                   <Image
                     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-                    className={`duration-300 w-16 laptop:w-24 ${currentImage === index && "scale-125"}`}
+                    className={`duration-300  w-16 laptop:w-24 ${currentImage === index && "scale-125"}`}
                     src={imageUrl ?? ""}
                     width={100}
                     height={800}
@@ -71,7 +89,7 @@ const Product = () => {
             <h1 className=" text-lg font-bold">{title}</h1>
             <span className=" text-md font-bold">Price</span>
             <p className="scrollbar text-sm text-primary-dark h-full max-h-[350px] overflow-y-auto">
-              ${(price / 100).toFixed(2)}
+              {priceFormatter(price)}
             </p>
             <span className=" text-md font-bold">Description:</span>
             <p className="scrollbar text-sm text-primary-darker h-full max-h-[350px] overflow-y-auto">
@@ -79,8 +97,8 @@ const Product = () => {
             </p>
           </article>
           {/* button container */}
-          <div className=" flex justify-end  w-full laptop:absolute bottom-0 bg-secondary laptop:py-8 px-6 py-6 laptop:text-md text-xs font-bold gap-7">
-            <div className="flex items-center ">
+          <div className=" flex  justify-between  w-full laptop:absolute bottom-0 bg-secondary laptop:py-8 px-6 py-6 laptop:text-md text-xs font-bold gap-7">
+            <div className="flex items-center justify-center ">
               <button className="relative w-8 h-8 flex items-center justify-center bg-cta-danger">
                 <AiOutlineMinus
                   className="text-white text-center w-5 h-6"
@@ -99,14 +117,13 @@ const Product = () => {
                 />
               </button>
             </div>
-            <div className="flex laptop:gap-7 gap-4 w-[315px]">
-              <button className="bg-cta laptop:w-fit w-full rounded-xl flex items-center justify-center py-2 laptop:px-7 bottom-12 right-8">
-                Add to cart
-              </button>
-              <button className="bg-cta laptop:w-fit w-full rounded-xl flex items-center justify-center py-2 laptop:px-7 bottom-12 right-8">
-                Buy
-              </button>
-            </div>
+
+            <button className="bg-cta rounded-xl w-1/2 flex items-center justify-center py-2 laptop:px-7 bottom-12 right-8">
+              Add to cart
+            </button>
+            <button className="bg-cta rounded-xl w-1/2 flex items-center justify-center py-2 laptop:px-7 bottom-12 right-8">
+              Buy
+            </button>
           </div>
           {/*End of button container */}
         </div>
