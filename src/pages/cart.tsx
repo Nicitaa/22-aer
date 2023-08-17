fb5c643d698b5e4a510e1d6e2ea9319b327eedae
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-08-17 14:30:51 +0300

    column order fixed on mobile

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,18 +1,17 @@
-import Link from "next/link"
 import { CartItem } from "~/components"
 import { Button } from "~/components/ui"
 const Cart = () => {
   return (
     <div className="flex h-[85vh] flex-col items-center">
-      <section className="mx-auto laptop:h-[70vh] h-[85vh] my-0 w-full max-w-[1062px] bg-secondary  tablet:px-0 rounded-t-xl">
-        <div className="h-[65%] overflow-y-auto scrollbar">
-          <table className="bg-secondary rounded-xl border-b-[64px] border-transparent w-full">
+      <section className="mx-auto laptop:h-[70vh] h-[85vh] my-0 w-full max-w-[1062px] bg-secondary  tablet:px-0 rounded-xl">
+        <div className="h-[65%] overflow-y-auto scrollbar rounded-t-xl">
+          <table className="bg-secondary rounded-xl border-b-[64px] border-transparent rounded-t-xl w-full">
             <thead>
-              <tr className="text-sm tablet:text-md font-bold sticky top-0 bg-black">
-                <th className="py-8">Item</th>
-                <th className="py-8">Price</th>
-                <th className="py-8 tablet:block hidden">Quantity</th>
-                <th className="py-8">Subtotal</th>
+              <tr className="text-sm tablet:text-md font-bold sticky top-0 bg-black rounded-t-xl">
+                <th className="py-8 order-1">Item</th>
+                <th className="py-8 order-3 tablet:order-2 ">Price</th>
+                <th className="py-8 order-2 tablet:order-3">Quantity</th>
+                <th className="py-8 order-4 tablet:block hidden">Subtotal</th>
                 <th className=""></th>
               </tr>
             </thead>
