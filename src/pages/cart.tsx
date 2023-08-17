999d8c2b258baf82031be37087d7e5fb6d445430
Illia Komarskyi <102596775+ottakist@users.noreply.github.com>
2023-08-17 17:25:01 +0300

    gap provided

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,9 +2,9 @@ import { CartItem } from "~/components"
 import { Button } from "~/components/ui"
 const Cart = () => {
   return (
-    <div className="flex h-[85vh] flex-col items-center px-6 tablet:px-0">
-      <section className="mx-auto laptop:h-[70vh] h-[85vh] my-0 w-full max-w-[1062px] bg-secondary rounded-xl">
-        <div className="h-[65%] overflow-y-auto scrollbar rounded-t-xl">
+    <div className="flex h-[80vh] flex-col items-center px-6 tablet:px-0">
+      <section className="mx-auto h-[90%] my-0 w-full max-w-[1062px] bg-secondary rounded-xl">
+        <div className="tablet:h-[65%] h-[60%] overflow-y-auto scrollbar rounded-t-xl">
           <table className="bg-secondary rounded-xl border-b-[64px] border-transparent rounded-t-xl w-full">
             <thead>
               <tr className="text-sm tablet:text-md font-bold sticky top-0 bg-black rounded-t-xl">
@@ -25,18 +25,18 @@ const Cart = () => {
             </tbody>
           </table>
         </div>
-        <div className=" relative h-[35%] w-full text-xs tablet:text-sm font-bold rounded-b-xl">
-          <div className="">
-            <Button href="products" className="absolute tablet:left-8 top-8 left-3">
-              Back to products
-            </Button>
-            <Button variant={"danger"} className="absolute text-white  top-8 tablet:right-8 right-3">
+        <div className="tablet:h-[35%] h-[40%] min-h-[180px] w-full text-xs tablet:text-sm font-bold rounded-b-xl flex flex-row justify-between tablet:px-8 px-3 tablet:py-8 py-3">
+          <div>
+            <Button href="products">Back to products</Button>
+          </div>
+          <div className=" flex justify-between  flex-col">
+            <Button variant={"danger"} className="text-white  self-start ">
               Clear Cart
             </Button>
+            <Button variant={"success"} className="text-white  self-end">
+              Submit
+            </Button>
           </div>
-          <Button variant={"success"} className="absolute  bottom-12 tablet:right-8 right-3">
-            Submit
-          </Button>
         </div>
       </section>
     </div>
