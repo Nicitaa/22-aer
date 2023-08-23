3e5097ef5ecc0768e52d861d58f5ed47ec9970a7
Nicitaa <icpcsenondaryemail@gmail.com>
2023-08-23 19:41:09 +0200

    aspect-ratio

--- src/components/pages/root/Slider3D.tsx
+++ src/components/pages/root/Slider3D.tsx
@@ -45,7 +45,7 @@ export function Slider3D({
           image.classList.remove("slide-image-active")
         })
 
-        document.querySelectorAll(".slider-image")[index]?.classList.add("slide-image-active")
+        document.querySelectorAll(".slider-image")[index]?.classList.add("slide-image-active", "aspect-video")
       })
 
       setCurrentSlide(index)
@@ -129,7 +129,7 @@ function Images(data: { slideTo: (index: number) => void; array: ITopSales[] }) 
             data.slideTo(index)
           }}
           key={index}
-          className={`image-wrapper flex-none grid`}>
+          className={`image-wrapper aspect-video flex-none grid`}>
           <Image
             placeholder="blur"
             blurDataURL={image.imgSrc}
@@ -137,7 +137,8 @@ function Images(data: { slideTo: (index: number) => void; array: ITopSales[] }) 
             height={"320"}
             src={image.imgSrc}
             alt="image"
-            className={`slider-image w-48 object-cover cursor-pointer ${index === Math.floor(data.array.length / 2) ? "slide-image-active" : ""
+            className={`slider-image transition-all duration-1000 aspect-video w-48 object-cover cursor-pointer
+             ${index === Math.floor(data.array.length / 2) ? "slide-image-active aspect-video" : ""
               }`}
             key={image.imgSrc}
           />
