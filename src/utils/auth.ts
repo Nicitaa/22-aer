206bf5a0d4756fbd76088e54bc84a1cf6489285a
Arifm6 <arifmassih6@gmail.com>
2023-09-12 13:38:04 -0400

    combined validation functions and messages

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,50 +11,35 @@ export const signInSchema = z.object({
 export const recoverAccountSchema = z.object({
   email: z.string().email(),
 })
-//returns true if email valid, false otherwise
-function validateEmail(email: string): boolean {
+function validateEmail(email: string): string {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-  return emailRegex.test(email)
-}
-function validateEmailMessage(email: string): string {
-  if (!validateEmail(email)) {
+  if (!email) {
+    return "Email is required"
+  } else if (!emailRegex.test(email)) {
     return "Please enter a valid email address"
   }
   return ""
 }
-function validatePassword(password: string): boolean {
+function validatePassword(password: string): string {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
-  return passwordRegex.test(password)
-}
-function validatePasswordMessage(password: string): string {
-  if (password.length === 0) {
+  if (!password) {
     return "Password is required"
-  } else if (!validatePassword(password)) {
+  } else if (!passwordRegex.test(password)) {
     return "Password requires a number, lowercase letter, uppercase letter, and must be atleast 8 characters long"
   }
   return ""
 }
-function validateRepeatPassword(password: string, repeatPassword: string): boolean {
-  if (!validatePassword(repeatPassword) || password !== repeatPassword) {
-    return false
-  }
-  return true
-}
-function validateRepeatPasswordMessage(password: string, repeatPassword: string): string {
-  if (repeatPassword.length === 0) {
+function validateRepeatPassword(password: string, repeatPassword: string): string {
+  if (!repeatPassword) {
     return "Repeat password is required"
   }
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
-  if (password !== repeatPassword) {
+  else if (password !== repeatPassword) {
     return "The password and repeat password must be the same"
   }
   return ""
 }
 export {
   validateEmail,
-  validateEmailMessage,
   validatePassword,
-  validatePasswordMessage,
   validateRepeatPassword,
-  validateRepeatPasswordMessage,
 }
